b15443f63e7ce2941ce8a3eee25b4bb3800f500e
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-19 13:22:58 +0300

    Initial Commit

---
+++ ContactList ContactList ContactList ContactList2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList ContactList ContactList ContactList2
+//
+//  Created by Elenka on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
